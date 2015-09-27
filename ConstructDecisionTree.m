99df87da59c75a9c2c295fdf9d768b42bf6d2cd7
rajamanohar <rajamanohar.mummidi@gmail.com>
2015-09-27 12:57:04 -0400

    commting creating child node changes

--- ConstructDecisionTree.m
+++ ConstructDecisionTree.m
@@ -7,30 +7,10 @@ SampleData=TrainData;
 [SampleData NodeValue NodeIdx Entropies]=DetermineNodeValue(SampleData,TrainData,Attributes,dtr,0)
 dtr=createOrUpdateTree(dtr,NodeValue,NodeValue,0);
 
-binMaxes=FindBinMaxes(SampleData,NodeIdx,TrainData);
-  
-% ConstrcutChildNodes();
-%  check class labels data for each bin 
+[binMaxes,binEndIndexs]=FindBinMaxes(SampleData,NodeIdx,TrainData);
+ ConstructChildNodes(dtr,NodeValue,binMaxes,binEndIndexs,SampleData);
 
-for binIndex=1:length(binMaxes)
-startIndex=1;
-BinClasses=C(startIndex:ia(binIndex,1),5);
-BinClasses=cell2mat(BinClasses);
-BinClasses=sortrows(BinClasses);
-[binClassDist,binClassEnds,binClassVals] = unique(BinClasses,'last','rows');
-end
 
-if length(binClassDist)==1
-    if binClassDist==1
-        classLabel='SETOSA';
-    elseif binClassDist==2
-        classLabel='OTHERS';
-        
-        dtr=createOrUpdateTree(dtr,classLabel,NodeValue,binMaxes(NodeIdx,1));
-    end
-else length(binClassDist)>1
-    
-end
 
 
 
