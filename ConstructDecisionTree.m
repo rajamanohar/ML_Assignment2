b2f87929511c6f196feb387b42ce3c325fb536ff
rajamanohar <rajamanohar.mummidi@gmail.com>
2015-09-27 12:30:55 -0400

    finding child nodes commented

--- ConstructDecisionTree.m
+++ ConstructDecisionTree.m
@@ -12,25 +12,25 @@ binMaxes=FindBinMaxes(SampleData,NodeIdx,TrainData);
 % ConstrcutChildNodes();
 %  check class labels data for each bin 
 
-for binIndex=1:length(binMaxes)
-startIndex=1;
-BinClasses=C(startIndex:ia(binIndex,1),5);
-BinClasses=cell2mat(BinClasses);
-BinClasses=sortrows(BinClasses);
-[binClassDist,binClassEnds,binClassVals] = unique(BinClasses,'last','rows');
-end
-
-if length(binClassDist)==1
-    if binClassDist==1
-        classLabel='SETOSA';
-    elseif binClassDist==2
-        classLabel='OTHERS';
-        
-        dtr=createOrUpdateTree(dtr,classLabel,NodeValue,binMaxes(NodeIdx,1));
-    end
-else length(binClassDist)>1
-    
-end
+% for binIndex=1:length(binMaxes)
+% startIndex=1;
+% BinClasses=C(startIndex:ia(binIndex,1),5);
+% BinClasses=cell2mat(BinClasses);
+% BinClasses=sortrows(BinClasses);
+% [binClassDist,binClassEnds,binClassVals] = unique(BinClasses,'last','rows');
+% end
+% 
+% if length(binClassDist)==1
+%     if binClassDist==1
+%         classLabel='SETOSA';
+%     elseif binClassDist==2
+%         classLabel='OTHERS';
+%         
+%         dtr=createOrUpdateTree(dtr,classLabel,NodeValue,binMaxes(NodeIdx,1));
+%     end
+% else length(binClassDist)>1
+%     
+% end
 
 
 
